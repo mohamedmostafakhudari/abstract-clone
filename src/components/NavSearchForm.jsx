7e9835de995693d65455915cd6f4dc2039c1085c
mohamedmostafakhudari <https://github.com/mohamedmostafakhudari>
2023-09-23 22:41:01 +0300

    Add the same logic to the nav search form

--- src/components/NavSearchForm.jsx
+++ src/components/NavSearchForm.jsx
@@ -5,6 +5,14 @@ import clsx from "clsx";
 import SearchInput from "./SearchInput";
 
 const NavSearchForm = ({ type, onClose, className }) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  function handleSearch(e) {
+    if (e.type === "click" || (e.type === "keydown" && e.key === "Enter")) {
+      e.preventDefault();
+      console.log(searchTerm);
+      setSearchTerm("");
+    }
+  }
   return (
     <form
       action="/"
@@ -12,7 +20,12 @@ const NavSearchForm = ({ type, onClose, className }) => {
       className={clsx("bg-white text-base text-black", className)}
     >
       <Container className={`flex items-center py-5`}>
-        <SearchInput className="flex-1" />
+        <SearchInput
+          className="flex-1"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onClick={handleSearch}
+          onKeyDown={handleSearch}
+        />
         <div onClick={onClose} className="group cursor-pointer p-4">
           <GrFormClose className="min-w-[16px] shrink-0 text-2xl duration-200 ease-in-out group-hover:scale-125" />
         </div>
